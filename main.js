ba9d9d617c322ce7a959aad39527f5ada3830779
Lê Văn Lợi <37243606+levanloi94@users.noreply.github.com>
2021-09-18 22:43:33 +0700

    deploy on heroku

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ router.render = (req, res) => {
 };
 
 // Use default router
-const PORT = process.eventNames.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 server.use('/api', router);
 server.listen(PORT, () => {
   console.log('JSON Server is running');
